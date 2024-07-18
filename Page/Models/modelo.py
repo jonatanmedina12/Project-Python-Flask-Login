0f4cd208405437884f5c236831e0175aca7d0b94
jonatanmedina12 <136943363+jonatanmedina12@users.noreply.github.com>
2024-07-17 22:26:18 -0500

    V1.0.3
    V1.0.3

---
+++ Page/Models/modelo.py
@@ -0,0 +1,36 @@
+from Page import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  #'primary_key'
+    username = db.Column(db.String(60), unique=True, nullable=False)
+    password = db.Column(db.Text, nullable=False)
+    is_active = db.Column(db.Integer, nullable=False, default=1)
+    created_on = db.Column(db.DateTime, default=datetime.now)  # DateTime'
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<user:{self.username}>'
+
+
+class Tareas(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)  # 'primary_key'
+    title = db.Column(db.String(100), nullable=False)
+    desc = db.Column(db.Text)
+    state = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Integer, nullable=False, default=1)
+    created_on = db.Column(db.DateTime, default=datetime.now)  # 'DateTime'
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  #  'Integer' y 'ForeignKey'
+
+    def __init__(self, created_by, title, desc, state=False):
+        self.created_by = created_by
+        self.title = title
+        self.desc = desc
+        self.state = state
+
+    def __repr__(self):
+        return f'<tarea:{self.title}>'
