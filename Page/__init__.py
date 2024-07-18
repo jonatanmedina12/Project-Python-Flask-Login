4f4dbb4acf78e286246d2860fc8ac731ec332e97
jonatanmedina12 <136943363+jonatanmedina12@users.noreply.github.com>
2024-07-18 00:04:08 -0500

    V1.0.4
    V1.0.4

--- Page/__init__.py
+++ Page/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
-from .Controller import auth_vista
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
 
 def create_app():
     app = Flask(__name__)
@@ -11,7 +9,7 @@ def create_app():
     app.config.from_mapping(
         DEBUG=True,
         SECRET_KEY='Dev',
-        SQLALCHEMY_DATABASE_URI="sqlite:///Tareas.db"
+        SQLALCHEMY_DATABASE_URI="sqlite:///tareas.db"
     )
 
     db.init_app(app)
@@ -20,13 +18,12 @@ def create_app():
         from .Models import modelo  # Importar modelos dentro del contexto de la aplicación
         db.create_all()  # Crear todas las tablas definidas en los modelos
 
-    # Registrar Blueprint
-    app.register_blueprint(auth_vista.bp)
+        from .Controller import auth_vista, vista
+        app.register_blueprint(auth_vista.bp)
+        app.register_blueprint(vista.bp)
 
     @app.route("/")
     def index():
-
         return render_template('index.html')
-
 
     return app
